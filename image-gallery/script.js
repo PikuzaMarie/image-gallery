4bf0be92dbb38ccbcb721f476590931633425470
PikuzaMarie <pikuza.mari@gmail.com>
2024-09-29 18:41:01 +0300

    feat: add function to add images to page

---
+++ image-gallery/script.js
@@ -0,0 +1,28 @@
+const searchBtn = document.getElementById('searchBtn');
+const searchInput = document.getElementById('searchInput');
+const imagesContainer = document.getElementById('imagesContainer');
+
+searchBtn.addEventListener('click', function() {
+    const query = searchInput.value;
+    searchImages(imagesContainer, query);
+});
+
+function searchImages(container, query) {
+    const url = `/.netlify/functions/unsplash-search?=${query}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            container.innerHTML = ''; // Очистим контейнер перед новым поиском
+
+            data.results.forEach (image => {
+                const img = document.createElement('img');
+                img.src = image.urls.small;
+                container.appendChild(img);
+            })
+        })
+        .catch(error => {
+            console.error('Error fetching photos:', error);
+        });
+}
+
