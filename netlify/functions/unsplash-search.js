70e3b26bda3ef20ce6f963c0f6eb04b4e37b633e
PikuzaMarie <pikuza.mari@gmail.com>
2024-09-29 19:37:10 +0300

    docs: add dotenv dependency

--- netlify/functions/unsplash-search.js
+++ netlify/functions/unsplash-search.js
@@ -1,12 +1,14 @@
 const fetch = require('node-fetch');
 require('dotenv').config();
 
+const accessKey = process.env.UNSPLASH_ACCESS_KEY;
+
 exports.handler = async function(event, context) {
     const query = event.queryStringParameters.query;
     const page = event.queryStringParameters.page || 1;
     const perPage = event.queryStringParameters.per_page || 10;
 
-    const url = `https://api.unsplash.com/search/photos?query=${query}&page=${page}&per_page=${perPage}&client_id=${process.env.UNSPLASH_ACCESS_KEY}`;
+    const url = `https://api.unsplash.com/search/photos?query=${query}&page=${page}&per_page=${perPage}&client_id=${accessKey}`;
 
     try {
         const response = await fetch(url);
