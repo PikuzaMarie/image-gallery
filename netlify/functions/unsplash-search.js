4ad7a67f98841912016f52c0a854411d43222ddb
PikuzaMarie <pikuza.mari@gmail.com>
2024-09-29 13:28:49 +0300

    feat: add unsplash search function to handle requests and responces

---
+++ netlify/functions/unsplash-search.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+exports.handler = async function(event, context) {
+    const query = event.queryStringParameters.query;
+    const page = event.queryStringParameters.page || 1;
+    const perPage = event.queryStringParameters.per_page || 10;
+
+    const url = `https://api.unsplash.com/search/photos?query=${query}&page=${page}&per_page=${perPage}&client_id=${process.env.UNSPLASH_ACCESS_KEY}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Ошибка при запросе к Unsplash API' }),
+        };
+    }
+};
